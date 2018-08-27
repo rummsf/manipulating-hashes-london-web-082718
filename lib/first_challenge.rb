ed080202eb44bc71a157ef21b4a06faa2c363a46
Irum F <irummf@gmail.com>
2018-08-27 18:25:25 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,11 +12,9 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if {|flavour| flavour === "strawberry" }
+contacts
+  
   contacts
 end
 
